83bdd73969c79d3aedab8d05330ad8d25115ea7a
Safietou Deme <safiedeme9@gmail.com>
2022-09-09 17:27:43 -0700

    print digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print all numbers from base 10
+ *
+ * Return: always 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d",i);
+	printf("/n");
+
+}
