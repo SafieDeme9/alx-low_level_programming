7bb9f09a0100092766af4b8c5873ee7db6611266
Safietou Deme <safiedeme9@gmail.com>
2022-09-11 14:13:29 -0700

    all combinations 2 digit num

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Print all all possible combinations of two-digit numbers.
+ *
+ *   * using putchar
+ *
+ *    * Numbers must be separated by ,, followed by a space
+ *
+ *     * Numbers should be printed in ascending order
+ *
+ *      *
+ *
+ *       * Return: always 0 if success
+ *
+ *        */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
