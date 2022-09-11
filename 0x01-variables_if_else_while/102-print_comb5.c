a0cef89f53c6ee9bceb2e7bcc1c09d613873e8a2
Safietou Deme <safiedeme9@gmail.com>
2022-09-11 14:23:14 -0700

    all combinations 2 digit num

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,13 +2,7 @@
 
 /**
  * main - Print all all possible combinations of two-digit numbers
- * 
- * using putchar
- * 
- * Numbers must be separated by ,, followed by space
- * 
- * Numbers should be printed in ascending order
- * 
+ *
  * Return: always 0 if success
  */
 
