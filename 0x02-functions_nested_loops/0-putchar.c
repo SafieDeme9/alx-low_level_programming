2175481c15166bf179a71d4c7925c7a1db235fb5
Safietou Deme <safiedeme9@gmail.com>
2022-09-13 15:29:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,15 @@
 
 /**
  * main - Write a program that prints _putchar, followed by a new line.
+ *  
  * Return : 0 if success
  */
 
 int main(void)
 {
 	char str[8] = "_putchar";
-	int i;
-	while(i<9)
+	int i = 0;
+	while (i < 9)
 	{
 		_putchar(str[i]);
 		i++;
