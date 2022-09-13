a947045d9e580606a64139f1a25f9087006f2690
Safietou Deme <safiedeme9@gmail.com>
2022-09-13 16:31:13 -0700

    is alpha and lower

---
+++ 0x02-functions_nested_loops/3-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * isalpha - checks wether the caracter is alpha or not
+ * Return: 0 or 1
+ */
+
+int islower(int c)
+{
+        return (c >= 97 && c <= 122 || (c >= 65 && c <= 90));
+}
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * islower - checks wether the caracter is lower or not
+ * Return: 0 or 1
+ */
+
+int islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
