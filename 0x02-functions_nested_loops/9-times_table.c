6de8e885172ac68deb55632804533f66a8d35864
Safietou Deme <safiedeme9@gmail.com>
2022-09-14 16:04:18 -0700

    nine times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,6 @@
 
 /**
  * times_table - prints the 9 times table starting with 0
- * @row: the indice of the line
- * @column: indice of the column 
- * @result: the result of the multiplication
  * Return: 0 if success
  */
 
@@ -17,7 +14,11 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			result = row * column;
-			if (result < 10)
+			if (column == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result < 10)
 			{
 				_putchar(' ');
 				_putchar('0' + result);
