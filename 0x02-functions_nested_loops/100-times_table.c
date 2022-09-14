e00df3e82a2885cf91356c246114d58282e4b429
Safietou Deme <safiedeme9@gmail.com>
2022-09-14 16:12:56 -0700

    print times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the 9 times table starting with 0
+ * Return: 0 if success
+ */
+
+int print_times_table(int n)
+{
+	int row, column, result;
+	
+	if (n < 0 || n > 15)
+		return;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+
+			if (column < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
