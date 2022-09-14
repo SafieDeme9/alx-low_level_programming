c2341e08b1f482b7e966dfd211ebc01b56961697
Safietou Deme <safiedeme9@gmail.com>
2022-09-13 18:29:11 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,10 @@ void print_to_98(int n)
 		n++;
 	}
 	while (n > 98)
-        {
-                printf("%d, ", n);
-                n--;
-        }
+	{
+		printf("%d, ", n);
+		n--;
+	}
 
 	printf("98");
 	putchar('\n');
