6ef53cecd38c932958afddb38727cf3eacf76c63
Safietou Deme <safiedeme9@gmail.com>
2022-09-13 17:57:51 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
-/** print_last_digit - return the last lady
- * @n : le numero a ecriver 
+/** 
+ * print_last_digit - return the last lady
+ * @n: le numero a ecriver 
  * Return: 0 pas trop
  */
 int print_last_digit(int n)
 {
 	int last;
+	
 	if (n > 0)
 	{
 		last = n % 10;
