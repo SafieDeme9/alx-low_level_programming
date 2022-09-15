9a630690c57d20b87ca581345c30ea9c5e7086c1
Safietou Deme <safiedeme9@gmail.com>
2022-09-15 15:22:22 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+
+}
