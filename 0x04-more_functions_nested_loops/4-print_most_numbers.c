b57543ce30007fefcda5de2a804db81faa4a553b
Safietou Deme <safiedeme9@gmail.com>
2022-09-15 15:33:20 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
- * print_numbers - print the numbers from 0 to 9
- * Return: 0
+ * print_most_numbers - prints numbers
+ * Return:0
  */
+
 void print_most_numbers(void)
 {
 	char i = 0;
-	
+
 	while (i <= 9)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar('0' + i);
 		}
-		
+
 		i++;
 	}
 	_putchar('\n');
