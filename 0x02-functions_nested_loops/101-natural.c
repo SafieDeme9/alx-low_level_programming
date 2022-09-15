545d1cd3f0a3df6aa2da3bc492a4d089c4667739
Safietou Deme <safiedeme9@gmail.com>
2022-09-14 17:20:32 -0700

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,8 +3,7 @@
 
 /**
  * main - corps of the program
- * Description:  computes and prints the sum of all the multiples of 3 or 5 
- * below 1024 (excluded), followed by a new line.
+ * Description: computes and prints the sum of all the multiples of 3 amd 5
  * Return: 0 if success
  */
 
