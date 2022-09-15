44b7171a21eb99311922eb992804f89a2a4f30e8
Safietou Deme <safiedeme9@gmail.com>
2022-09-15 15:46:03 -0700

    more numbers 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - print the numbers from 0 to 9
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	char j, k;
+
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j > 9)
+			{
+				_putchar('1');
+				k = j % 10;
+			}
+			_putchar('0' + j);
+		}
+		
+		_putchar('\n');
+		i++;
+	}
+
+}
