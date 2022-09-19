cc064e294a13e89a9d3bc96246f5932c00411440
Safietou Deme <safiedeme9@gmail.com>
2022-09-19 13:48:57 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout.
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(str);
+	for (i = 0; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
