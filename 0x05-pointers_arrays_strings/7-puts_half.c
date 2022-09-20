526baec2e4f4886875a4950f27c2205d25c15a67
Safietou Deme <safiedeme9@gmail.com>
2022-09-19 17:14:16 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts_half - the second half of the string
- * If the number of characters is odd, the function should print 
+ * If the number of characters is odd, the function should print
  * the last n characters of the string
  * @str: string reference pointer
  * Return: void
