10cb0f227150b1468c823c5d255dd0dadf988aa8
Safietou Deme <safiedeme9@gmail.com>
2022-09-19 17:13:13 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,5 +8,36 @@
  */
 void puts_half(char *str)
 {
+	int len;
 
+	len = _strlen(str);
+	if (len % 2 == 0)
+	{
+		len = len / 2;
+	}
+	else
+	{
+		len = (len + 1) / 2;
+	}
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: the string to check
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
