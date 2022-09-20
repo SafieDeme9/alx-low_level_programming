670530fe3a031ce09db65adba4068f55c033b409
Safietou Deme <safiedeme9@gmail.com>
2022-09-19 17:28:51 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,8 @@
  * Write a function that copies the string pointed to by
  * src, including the terminating null byte (\0), to the
  * buffer pointed to by dest.
+ * @dest: destination
+ * @src: source
  * Return: the pointer to dest
  */
 
