ddf5dc27ac5879f46d80bdae59fb249f74964ab7
Safietou Deme <safiedeme9@gmail.com>
2022-09-19 17:01:56 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - rints every other character of a string, starting with 
+ * puts2 - rints every other character of a string, starting with
  * the first character, followed by a new line.
  * @str: string reference pointer
  * Return: void
@@ -9,6 +9,7 @@
 void puts2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
