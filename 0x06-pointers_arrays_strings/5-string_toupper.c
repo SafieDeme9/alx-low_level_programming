ba4e15630785da9d170750176ed7ec1dcc71def0
Safietou Deme <safiedeme9@gmail.com>
2022-09-22 16:31:05 -0700

    lowercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,12 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i++])
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
+		i++;
 	}
 
-	return (str)i;
+	return (str);
 }
