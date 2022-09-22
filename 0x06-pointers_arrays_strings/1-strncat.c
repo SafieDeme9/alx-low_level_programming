2b3dff7207d9693381226812fc1e727e7fe057ea
Safietou Deme <safiedeme9@gmail.com>
2022-09-22 15:22:46 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,14 +6,16 @@
  * @n: limit
  * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, len = 0;
 
-	while (dest[i])
+	while (dest[i++])
 		len++;
+
 	for (i = 0; src[i] && i < n; i++)
-		dest[len] = src[i];
+		dest[len++] = src[i];
 
 	return (dest);
 }
