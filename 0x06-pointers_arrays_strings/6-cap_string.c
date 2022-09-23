b951e5c86b0e59765518b5a2522672c1ab7a5a7d
Safietou Deme <safiedeme9@gmail.com>
2022-09-22 17:14:42 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: string 
- * Return: returns a character
+ * Return: returns a capitalized string
  */
 char *cap_string(char *str)
 {
