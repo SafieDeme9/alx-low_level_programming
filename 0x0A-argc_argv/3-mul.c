fbc60ca3ecdd5235d979246b2f30b6ccbcecd805
Safietou Deme <safiedeme9@gmail.com>
2022-09-30 16:59:35 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,7 @@
  * main - main function
  * @argc: number of command line arguments
  * @argv: array of size argc
- * Return: 0
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv[])
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 	if (argc > 2)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 		return (0);
 	}
 	else
