36d6f2447d4e8ca384d9aa622f13f970845a9932
Safietou Deme <safiedeme9@gmail.com>
2022-09-30 11:36:09 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
