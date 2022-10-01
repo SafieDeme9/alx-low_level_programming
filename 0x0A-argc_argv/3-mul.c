529f85338283554d805d20a98f2180f89c7f028e
Safietou Deme <safiedeme9@gmail.com>
2022-09-30 17:32:40 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,9 +14,6 @@ int main(int argc, char *argv[])
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
-	else
-	{
-		printf("Error \n");
-		return (1);
-	}
+	printf("Error \n");
+	return (1);
 }
