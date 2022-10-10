5a99a9b92829deea7ed367410dffffa06c4037b7
SafieDeme9 <safiedeme9@gmail.com>
2022-10-10 12:35:37 -0400

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,10 +6,7 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- */
-
-/**
-*dog_t - Typedef for struct dog
+ * dog_t - Typedef for struct dog
 */
 typedef struct dog
 {
