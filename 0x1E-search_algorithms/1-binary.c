04c1190614a6330a23d4be81a27a38d8d9643a27
SafieDeme9 <safiedeme9@gmail.com>
2023-04-14 08:23:59 -0400

    task 1 corrected

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,9 +15,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	left = 0;
-	right = size - 1;
-	while (right >= left)
+	for (left = 0, right = size - 1; right >= left;)
 	{
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
