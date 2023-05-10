4b3007e8f92d7c18df998dca40778e103731e2b0
SafieDeme9 <safiedeme9@gmail.com>
2023-05-10 16:13:55 -0400

    task 0 fixed betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,7 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
@@ -19,8 +20,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (value == array[i])
 		return (i);
-
 	}
-	
+
 	return (-1);
 }
